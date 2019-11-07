9debac388107902132b32f1b9e4f4f6043edafe9
Fatemeh Pakpour <fatemehpakpour64@gmail.com>
2019-11-07 20:34:25 +0100

    added the variable

---
+++ main.js
@@ -0,0 +1,7 @@
+// variables HTML elements
+const btnWeather = document.querySelector("#button-search");
+const cityNameInput = document.querySelector("#city-name");
+const btnCurrentLocation = document.querySelector("#button-currentLocation");
+
+// Api Key form https://openweathermap.org/
+const apiKey = "0c6db5d597b4b735d735f6d9bedd78c2";
