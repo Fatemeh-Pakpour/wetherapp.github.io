83a48df08e4ae695386c2b150b33b96de6cb4831
Fatemeh Pakpour <fatemehpakpour64@gmail.com>
2019-11-07 21:18:40 +0100

    added the function for rendering the local location wethaer

--- main.js
+++ main.js
@@ -12,33 +12,55 @@ const apiKey = "0c6db5d597b4b735d735f6d9bedd78c2";
  * @param {*} cityName
  */
 function weatherInfo(cityName) {
-    fetch(
-      "https://api.openweathermap.org/data/2.5/weather?&q=" +
-        cityName +
-        "&units=metric&appid=" +
-        apiKey
-    )
-      .then(resp => resp.json())
-      .then(json => {
-        displayWeatherInfo(json);
-      });
-  }
-  function displayWeatherInfo(data) {
-    // weather icon
-  
-    const iconCode = data.weather[0].icon;
-    const iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
-    document.querySelector("#weather-icon").setAttribute("src", iconUrl);
-  
-    // location
-    document.querySelector("#location").innerHTML = data.name;
-  
-    // temperature
-    document.querySelector("#temp").innerHTML = `${data.main.temp} &degC`;
-  
-    // wind speed
-    const wind = data.wind.speed;
-    //sunset & sunrise
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?&q=" +
+      cityName +
+      "&units=metric&appid=" +
+      apiKey
+  )
+    .then(resp => resp.json())
+    .then(json => {
+      displayWeatherInfo(json);
+    });
+}
+/**
+ *
+ *
+ * @param {*} lat =>latitude
+ * @param {*} lon =>longitude
+ */
+function weatherInfoCurrentLocation(lat, lon) {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&units=metric&appid=" +
+      apiKey
+  )
+    .then(resp => resp.json())
+    .then(json => {
+      localStorage.clear();
+      localStorage.setItem("city", JSON.stringify(json));
+      displayWeatherInfo(json);
+    });
+}
+function displayWeatherInfo(data) {
+  // weather icon
+
+  const iconCode = data.weather[0].icon;
+  const iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+  document.querySelector("#weather-icon").setAttribute("src", iconUrl);
+
+  // location
+  document.querySelector("#location").innerHTML = data.name;
+
+  // temperature
+  document.querySelector("#temp").innerHTML = `${data.main.temp} &degC`;
+
+  // wind speed
+  const wind = data.wind.speed;
+  //sunset & sunrise
   const secSunrise = data.sys.sunrise;
   const secSunset = data.sys.sunset;
 
@@ -53,16 +75,16 @@ function weatherInfo(cityName) {
       hour: "2-digit",
       minute: "2-digit"
     });
-} 
+  }
   const timeSunrise = convertTime(secSunrise);
   const timeSunset = convertTime(secSunset);
-   // cloudy
-   const cloudsData = data.weather[0].description;
-   const cloudiness = cloudsData.charAt(0).toUpperCase() + cloudsData.slice(1);
-   const tableDataLi = document.createElement("li");
-   const tableDataUl = document.querySelector(".table .table-data");
-   const tableTitle = document.querySelector(".table .table-title");
-  
+  // cloudy
+  const cloudsData = data.weather[0].description;
+  const cloudiness = cloudsData.charAt(0).toUpperCase() + cloudsData.slice(1);
+  const tableDataLi = document.createElement("li");
+  const tableDataUl = document.querySelector(".table .table-data");
+  const tableTitle = document.querySelector(".table .table-title");
+
   tableTitle.style.display = "block";
   tableDataUl.innerHTML = "";
   tableDataLi.innerHTML = `
@@ -74,27 +96,30 @@ function weatherInfo(cityName) {
             </ul>
         `;
   tableDataUl.appendChild(tableDataLi);
+  // display the location on the map
+  const latitude = data.coord.lat;
+  const longitude = data.coord.lon;
+  renderLocationOnGoogleMap(latitude, longitude);
+}
+cityNameInput.addEventListener("keyup", event => {
+  event.preventDefault();
+  if (event.keyCode === 13) {
+    btnWeather.click();
   }
-  cityNameInput.addEventListener("keyup", (event) => {
-    event.preventDefault();
-    if (event.keyCode === 13) {
-      btnWeather.click();
-    }
-    if (cityNameInput.value === "") {
-      alert("Enter your city name");
-    }
-    weatherInfo(cityNameInput.value);
-   
+  if (cityNameInput.value === "") {
+    alert("Enter your city name");
+  }
+  weatherInfo(cityNameInput.value);
+});
+btnCurrentLocation.addEventListener("click", () => {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    const currentLocation = {
+      longitude: position.coords.longitude,
+      latitude: position.coords.latitude
+    };
+    weatherInfoCurrentLocation(
+      currentLocation.latitude,
+      currentLocation.longitude
+    );
   });
-  btnCurrentLocation.addEventListener("click", () => {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      const currentLocation = {
-        longitude: position.coords.longitude,
-        latitude: position.coords.latitude
-      };
-      weatherInfoCurrentLocation(
-        currentLocation.latitude,
-        currentLocation.longitude
-      );
-    });
-  });
+});
