fb1ac281021f5825e2786ec0847dec8fa249ee44
Fatemeh Pakpour <fatemehpakpour64@gmail.com>
2019-11-07 20:37:14 +0100

    added the function for fetching api

--- main.js
+++ main.js
@@ -5,3 +5,21 @@ const btnCurrentLocation = document.querySelector("#button-currentLocation");
 
 // Api Key form https://openweathermap.org/
 const apiKey = "0c6db5d597b4b735d735f6d9bedd78c2";
+
+/**
+ *function get the weather information from fetching api
+ *
+ * @param {*} cityName
+ */
+function weatherInfo(cityName) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?&q=" +
+        cityName +
+        "&units=metric&appid=" +
+        apiKey
+    )
+      .then(resp => resp.json())
+      .then(json => {
+       console.log(json);
+      });
+  }
